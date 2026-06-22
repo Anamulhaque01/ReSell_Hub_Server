ea8e03551d6b555fecca2365ae10a6ff2681cb96
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-06-22 12:39:59 +0600

    Server Setup Completed

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("ReSell Hub Server Running");
+});
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
