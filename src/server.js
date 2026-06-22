da4bc92d31101e3ff9c59ba2f8f44adf40b84a2f
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-06-22 12:56:03 +0600

    Backend Middleware  Added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
 
 
 dotenv.config();
@@ -12,6 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+    "/api/auth",
+    authRoutes
+);
 
 
 app.get("/", (req, res) => {
